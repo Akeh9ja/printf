9b7f69e388cbe8a8e2fc85398c42a7fdfec7d7b8
Sommy20 <chi5chisom20@gmail.com>
2022-04-21 15:02:08 +0100

    Added to the description and corrected mistakes

--- print_custom.c
+++ print_custom.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+ /**
  * print_bigS - Non printable characters
  * (0 < ASCII value < 32 or >= 127) are
  * printed this way: \x, followed by the ASCII code
@@ -38,7 +38,7 @@ int print_bigS(va_list l, flags_t *f)
 	return (count);
 }
 
-/**
+ /**
  * print_rev - prints a string in reverse
  * @l: argument from _printf
  * @f: pointer to the struct flags that determines
@@ -63,7 +63,7 @@ int print_rev(va_list l, flags_t *f)
 	return (i);
 }
 
-/**
+ /**
  * print_rot13 - prints a string using rot13
  * @l: list of arguments from _printf
  * @f: pointer to the struct flags that determines
@@ -97,7 +97,7 @@ int print_rot13(va_list l, flags_t *f)
 	return (j);
 }
 
-/**
+ /**
  * print_percent - prints a percent
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags in which we turn the flags on
